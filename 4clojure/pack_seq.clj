11ea5c20aea2fb3d5583ae2b27ec6cd2eb6a7f6d
jaeyeon <jojaeyeon.kr@gmail.com>
2020-01-07 22:51:06 +0900

    drop

--- 4clojure/pack_seq.clj
+++ 4clojure/pack_seq.clj
@@ -1,1 +1,11 @@
 ;;Write a function which packs consecutive duplicates into sub-lists.
+
+;;(= (__ [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
+
+(fn [x]
+  (loop [in x out []]
+    (if (= in [])
+      out
+      (recur
+        (drop-while #(= (first in) %) in)
+        (conj out (take-while #(= (first in) %) in))))))
